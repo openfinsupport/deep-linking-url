1c8a57b791879176f91a3de376e24081893d0098
connormccafferty <connor.mccafferty@openfin.co>
2020-10-19 15:31:31 -0400

    fin.desktop.main and Application run-requested listeners added

--- js/index.js
+++ js/index.js
@@ -8,6 +8,66 @@ const init = async () => {
   }
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  init();
+const main = () => {
+  window.fin.desktop.main(async (args) => {
+    const win = fin.Window.getCurrentSync();
+    // handles subsequent launches
+    registerRunRequested();
+
+    if (args.url) {
+      const { url } = args.userAppConfigArgs;
+
+      let win = await fin.Window.create({
+        name: `openfin-template-window-${Date.now()}`,
+        url,
+        autoShow: true,
+      });
+
+      return await win.setAsForeground();
+    }
+
+    // if no args, it's a regular launch just show the window
+    return await win.show();
+  });
+};
+
+function registerRunRequested() {
+  const app = window.fin.Application.getCurrentSync();
+  if (app.listenerCount("run-requested") < 1) {
+    app
+      .addListener("run-requested", async (event) => {
+        if (event.userAppConfigArgs.url) {
+          const { url } = event.userAppConfigArgs;
+
+          let win = await fin.Window.create({
+            name: `openfin-template-window-${Date.now()}`,
+            url,
+            autoShow: true,
+          });
+
+          return await win.setAsForeground();
+        } else {
+          return (await app.getWindow()).show();
+        }
+      })
+      .then(() => console.log("run-requested listener registered"))
+      .catch((err) => console.log("error registering listener:", err));
+  }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  main();
+  await init();
+
+  const urlInput = document.getElementById("url-param-input");
+  const launchBtn = document.getElementById("launch-with-url");
+
+  // launchBtn.addEventListener("click", (e) => {
+  //   e.preventDefault();
+
+  //   launchBtn.href = `${location.href}?$$url=${encodeURIComponent(
+  //     urlInput.value
+  //   )}`;
+  //   urlInput.value = "";
+  // });
 });
