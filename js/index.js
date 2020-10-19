f06108d40a12798af1b9035df316f528e937fbd4
openfinsupport <61983940+openfinsupport@users.noreply.github.com>
2020-10-19 16:28:34 -0400

    commit to csb

--- js/index.js
+++ js/index.js
@@ -3,58 +3,11 @@ const init = async () => {
   if (window.fin) {
     const runtimeVersion = await window.fin.System.getVersion();
     version.innerText = runtimeVersion;
-    main();
   } else {
     version.innerText = "Not Applicable (you are running a browser)";
   }
 };
 
-function main() {
-  window.fin.desktop.main(async (args) => {
-    const win = fin.Window.getCurrentSync();
-    // handles subsequent launches
-    registerRunRequested();
-    if (args) {
-      const { url } = args;
-
-      let win = await fin.Window.create({
-        name: `openfin-template-window-${Date.now()}`,
-        url,
-        autoShow: true,
-      });
-
-      return await win.setAsForeground();
-    }
-
-    // if no args, it's a regular launch just show the window
-    return await win.show();
-  });
-}
-
-function registerRunRequested() {
-  const app = window.fin.Application.getCurrentSync();
-  if (app.listenerCount("run-requested") < 1) {
-    app
-      .addListener("run-requested", async (event) => {
-        if (event.userAppConfigArgs.url) {
-          const { url } = event.userAppConfigArgs;
-
-          let win = await fin.Window.create({
-            name: `openfin-template-window-${Date.now()}`,
-            url,
-            autoShow: true,
-          });
-
-          return await win.setAsForeground();
-        } else {
-          return (await app.getWindow()).show();
-        }
-      })
-      .then(() => console.log("run-requested listener registered"))
-      .catch((err) => console.log("error registering listener:", err));
-  }
-}
-
-document.addEventListener("DOMContentLoaded", async () => {
-  await init();
+document.addEventListener("DOMContentLoaded", () => {
+  init();
 });
