bfd1259f894cf6c44e56b32048c6a0c058bcd939
connormccafferty <connor.mccafferty@openfin.co>
2020-10-19 16:23:30 -0400

    working example with hard coded google link

--- js/index.js
+++ js/index.js
@@ -3,19 +3,19 @@ const init = async () => {
   if (window.fin) {
     const runtimeVersion = await window.fin.System.getVersion();
     version.innerText = runtimeVersion;
+    main();
   } else {
     version.innerText = "Not Applicable (you are running a browser)";
   }
 };
 
-const main = () => {
+function main() {
   window.fin.desktop.main(async (args) => {
     const win = fin.Window.getCurrentSync();
     // handles subsequent launches
     registerRunRequested();
-
-    if (args.url) {
-      const { url } = args.userAppConfigArgs;
+    if (args) {
+      const { url } = args;
 
       let win = await fin.Window.create({
         name: `openfin-template-window-${Date.now()}`,
@@ -29,7 +29,7 @@ const main = () => {
     // if no args, it's a regular launch just show the window
     return await win.show();
   });
-};
+}
 
 function registerRunRequested() {
   const app = window.fin.Application.getCurrentSync();
@@ -56,18 +56,5 @@ function registerRunRequested() {
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
-  main();
   await init();
-
-  const urlInput = document.getElementById("url-param-input");
-  const launchBtn = document.getElementById("launch-with-url");
-
-  // launchBtn.addEventListener("click", (e) => {
-  //   e.preventDefault();
-
-  //   launchBtn.href = `${location.href}?$$url=${encodeURIComponent(
-  //     urlInput.value
-  //   )}`;
-  //   urlInput.value = "";
-  // });
 });
